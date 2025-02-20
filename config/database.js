b14f5ce412fec9059f5d193420268255eeb55eb7
Alvin774 <alvin@archlinux.localdomain>
2025-02-20 10:29:49 +0800

    inI

--- config/database.js
+++ config/database.js
@@ -2,9 +2,18 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.SQL_DATABASE_URL, {
-  dialect: 'mysql',
-  logging: false, // Nonaktifkan logging SQL (opsional)
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,     // Nama database
+  process.env.DB_USER,     // Username database
+  process.env.DB_PASSWORD, // Password database
+  process.env.DATABASE_URL,
+
+  {
+    host: process.env.DB_HOST,  // Host MySQL, misal "localhost"
+    port: process.env.PORT,
+    dialect: 'mysql',
+    logging: false,             // Nonaktifkan logging SQL (opsional)
+  }
+);
 
 module.exports = sequelize;
