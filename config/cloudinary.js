82452dd356d695f10439b0a27630dca2461f885f
Alvin774 <alvin@archlinux.localdomain>
2025-02-20 21:36:46 +0800

    ho

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -6,7 +6,6 @@ cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME, // Contoh: 'mycloudname'
   api_key: process.env.CLOUDINARY_API_KEY,       // Contoh: '123456789'
   api_secret: process.env.CLOUDINARY_API_SECRET, // Contoh: 'abcdefg'
-  secure:true
 });
 
 module.exports = cloudinary;
